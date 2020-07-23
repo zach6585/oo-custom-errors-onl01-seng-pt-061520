a9a0bc4795ac65166d1d75d6ee702fc6909908a5
zachary mines <zmines@wesleyan.edu>
2020-07-23 18:39:07 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,10 +7,13 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != person 
+      raise PartnerError
     person.partner = self
   end
 
   class PartnerError < StandardError 
+    
   end 
 end
 
